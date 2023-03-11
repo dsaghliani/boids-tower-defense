178944222f5904e1e51a08d7b275818b93641ff0
Demetre Saghliani <saghliani.d@gmail.com>
2023-03-11 10:16:32 +0400

    Set up drone spawning. Create `rustfmt.toml`.

--- src/drones.rs
+++ src/drones.rs
@@ -1,5 +1,7 @@
 use crate::{GameConfig, Velocity};
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+use rand::Rng;
+use std::f32::consts;
 
 // ------------------------ Plugin
 
@@ -21,13 +23,54 @@ struct DroneBundle {
     drone: Drone,
     velocity: Velocity,
     #[bundle]
-    transform_bundle: TransformBundle,
+    sprite_bundle: SpriteBundle,
 }
 
 // ------------------------ Systems
 
-fn spawn(mut commands: Commands, settings: Res<GameConfig>) {
-    info!("Spawning {} drones.", settings.drone_count);
-    let drones_iter = (0..settings.drone_count).map(|_| DroneBundle::default());
-    commands.spawn_batch(drones_iter);
+fn spawn(
+    mut commands: Commands,
+    settings: Res<GameConfig>,
+    asset_server: Res<AssetServer>,
+    primary_win: Query<&Window, With<PrimaryWindow>>,
+) {
+    debug!("Spawning {} drones.", settings.drone_count);
+
+    let (win_width, win_height) = {
+        let primary_win = primary_win.single();
+        (primary_win.width(), primary_win.height())
+    };
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..settings.drone_count {
+        let texture = asset_server.load("boid_16.png");
+        let translation = Vec3::new(
+            rng.gen_range(-win_width / 2.0..win_width / 2.0),
+            rng.gen_range(-win_height / 2.0..win_height / 2.0),
+            0.0,
+        );
+        let rotation =
+            Quat::from_rotation_z(rng.gen_range(0.0..(consts::PI * 2.0)));
+
+        trace!(
+            "Spawning a `DroneBundle` at translation {} and rotation {} with the \
+                texture {:?}.",
+            translation,
+            rotation,
+            texture
+        );
+
+        commands.spawn(DroneBundle {
+            sprite_bundle: SpriteBundle {
+                texture,
+                transform: Transform {
+                    translation,
+                    rotation,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+    }
 }
